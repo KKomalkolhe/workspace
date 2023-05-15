bf0d3995b8a52384a833d39cd430e85cd6fe3457
Komalkolhe <komalkolhe@tredence.com>
2023-05-15 10:34:10 +0530

    first commit

---
+++ Master2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import requests
+import pandas as pd
+
+base_url = "https://globalmart-api.onrender.com"
+endpoint = "/mentorskool/v1/sales?offset=1&limit=100"
+headers = {"access_token": "fe66583bfe5185048c66571293e0d358"}
+
+url = base_url + endpoint
+
+final_list = []
+nxt = ""
+
+for i in range(5):
+    if nxt != "":
+        url = base_url + nxt
+        response = requests.get(url, headers = headers)
+        data = response.json()["data"]
+        nxt = response.json()["next"]
+
+        final_list.extend(data)
+
+df = pd.json_normalize(final_list)
+df
+
+
+# In[ ]:
+
+
+
+
